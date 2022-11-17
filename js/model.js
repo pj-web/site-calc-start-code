e16963c0a68b97d30f042f293be6a2fec1b2a3c1
pj-web <insta.pj.05@gmail.com>
2022-11-17 23:48:53 +0400

    Initial commit

---
+++ js/model.js
@@ -0,0 +1,15 @@
+let data = {
+    selectedProgram: 0.1,
+    programs: {
+        base: 0.1,
+        it: 0.047,
+        gov: 0.067,
+        zero: 0.12,
+    }
+};
+
+function getData() {
+    return {...data}
+}
+
+export {getData}
