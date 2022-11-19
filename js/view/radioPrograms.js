4306d4647129c55e82f78700f9fb965edb2596e2
pj-web <insta.pj.05@gmail.com>
2022-11-19 12:09:29 +0400

    Update radioPrograms.js

--- js/view/radioPrograms.js
+++ js/view/radioPrograms.js
@@ -1,6 +1,22 @@
 function init(getData) {
     console.log('Init programs');
     console.log(getData());
+
+    const {base, it, gov, zero} = getData().programs;
+    console.log(base);
+    console.log(it);
+
+    //Set program rates in radio buttons
+    document.querySelector('#base-value').value = base;
+    document.querySelector('#it-value').value = it;
+    document.querySelector('#gov-value').value = gov;
+    document.querySelector('#zero-value').value = zero;
+    
+    //Show program rates on page
+    document.querySelector('#base-text').innerText = base * 100 + '%';
+    document.querySelector('#it-text').innerText = it * 100 + '%';
+    document.querySelector('#gov-text').innerText = gov * 100 + '%';
+    document.querySelector('#zero-text').innerText = zero * 100 + '%';
 }
 
 export default init;
