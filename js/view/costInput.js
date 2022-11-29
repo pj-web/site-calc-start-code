bc3b1d29a542cf4e0851cce7dd4a4652d0a53b73
pj-web <insta.pj.05@gmail.com>
2022-11-29 23:06:29 +0400

    Update costInput.js

--- js/view/costInput.js
+++ js/view/costInput.js
@@ -1,5 +1,5 @@
 function init(getData) {
-    console.log('FIRE!');
+    const data = getData();
     const input = document.querySelector('#input-cost');
 
     const settings = {
@@ -9,11 +9,33 @@ function init(getData) {
     }
     
     const cleaveInput = new Cleave(input, settings);
-    cleaveInput.setRawValue(getData().cost);
+    cleaveInput.setRawValue(data.cost);
 
     input.addEventListener('input', function () {
-        
-    })
+        const value = +cleaveInput.getRawValue(); 
+        console.log(value);
+
+        // Проверка на минимальную и максимальную цену
+        if (value < data.minPrice || value > data.maxPrice) {
+            input.closest('.param__details').classList.add('param__details--error');
+        }
+        if (value >= data.minPrice && value <= data.maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+        }
+    });
+
+    input.addEventListener('change', function () {
+        const value = +cleaveInput.getRawValue();
+
+        if (value > data.maxPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleaveInput.setRawValue(data.maxPrice);
+        }
+        if (value < data.minPrice) {
+            input.closest('.param__details').classList.remove('param__details--error');
+            cleaveInput.setRawValue(data.minPrice);
+        }
+    });
 }
 
 export default init;
